5a7e388ed6d539d85c56d9cb4f1fbae52a194606
Harold Ochoa <harold2828@hotmail.com>
2024-07-01 11:47:52 -0500

    Modify the order of generate the information

--- Models/AssistantTool.py
+++ Models/AssistantTool.py
@@ -9,13 +9,12 @@ class AssistantTool:
     
     def execute(self, project:dict, solar_panel:dict, inverter:dict) -> dict:
         sizing = {
-            "project_id":project['id'],
+            "project_id":project['  '],
             "total_number_of_panels":None,
             "panels_in_parallel":None,
             "panels_in_series":None,
             "panel_reference":solar_panel['id'],
-            "inverter_reference":inverter['id'],
-            "average_solar_hours":None
+            "inverter_reference":inverter['id']
         }
 
         performance_ratio = 0.85
@@ -26,28 +25,14 @@ class AssistantTool:
             #Units per day
             total_energy_day = total_energy/365 #kWh
 
-            parameters = {
-                "start":"20230101",
-                "end":"20240101",
-                "latitude":project['latitude'],
-                "longitude":project['longitude'],
-                "community":"re",
-                "parameters":"ALLSKY_SFC_SW_DWN",
-                "format":"json",
-                "header":"true",
-                "time-standard":"lst"
-            }
-
-            average_solar_hours = self.nasa_power.get_average_solar_hours(parameters)
-
-            peak_power = total_energy_day/average_solar_hours * performance_ratio
+            peak_power = total_energy_day/project['average_solar_hours'] * performance_ratio
 
             nominal_power = solar_panel['Impo'] * solar_panel['Vmpo']
 
             sizing["total_number_of_panels"] = math.ceil(peak_power/(nominal_power*1000))
             sizing["panels_in_parallel"]  = math.ceil(inverter['Vdcmax']/solar_panel['Voco'])
             sizing["panels_in_series"] = math.ceil(inverter['Idcmax']/solar_panel['Isco'])
-            sizing['average_solar_hours'] = average_solar_hours
         except:
             print("Something was wrong")
         return sizing
+   
